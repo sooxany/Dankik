946702f50748383cc5fb57c2281ac15b41d416b1
sooxany <soojin0929@naver.com>
2023-05-12 01:58:20 +0900

    진짜로 ...

---
+++ AML/src/main/java/com/lec/spring/AmlApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmlApplication.class, args);
+	}
+
+}
