32433820a61a8326806310100a315c6731e52fb2
sooxany <soojin0929@naver.com>
2023-05-19 22:48:32 +0900

    s

--- AML/src/main/java/com/example/demo/controller/HomeController.java
+++ AML/src/main/java/com/example/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 	//기본페이지 요청 메소드
 	@GetMapping("/")
 	public String index() {
-		return "index.html"; // index라는 html 파일을 띄우겠다/ templates 폴더의 index.html을 찾아감
+		return "index"; // index라는 html 파일을 띄우겠다/ templates 폴더의 index.html을 찾아감
 	}
 	
 
