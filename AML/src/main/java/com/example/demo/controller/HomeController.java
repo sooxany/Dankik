ecf99fd4f8f2e5a32f9767e16e36584756bd04a4
sooxany <soojin0929@naver.com>
2023-05-21 14:09:12 +0900

    .

--- AML/src/main/java/com/example/demo/controller/HomeController.java
+++ AML/src/main/java/com/example/demo/controller/HomeController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	//기본페이지 요청 메소드
-	@GetMapping("/")
-	public String index() {
-		return "index"; // index라는 html 파일을 띄우겠다/ templates 폴더의 index.html을 찾아감
-	}
-	
+	@GetMapping("/home")
+	public String home() {
+		return "home"; // index라는 html 파일을 띄우겠다/ templates 폴더의 index.html을 찾아감
+	}	
 
 }
---
+++ AML/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+	//회원가입 페이지 출력 요청
+	@GetMapping("member/save")
+	public String saveForm() {
+		return "save";
+	}
+
+}
